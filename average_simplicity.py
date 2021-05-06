c94504bd2be4a6052bdd19b2c17597528cfe9688
Christopher Plumberg <plumberg.1@osu.edu>
2021-05-06 11:13:10 -0500

    Update average_simplicity.py

--- average_simplicity.py
+++ average_simplicity.py
@@ -14,6 +14,7 @@ if len(sys.argv) > 3:
 else:
     outfilename = sys.argv[1]
     data = np.array([np.loadtxt(file, usecols=(1,2,3,4,5,6)) for file in get_filenames(sys.argv[2])])
+    print(data.shape)
     data = np.mean(data, axis=0)
     np.savetxt(outfilename, data)
 
