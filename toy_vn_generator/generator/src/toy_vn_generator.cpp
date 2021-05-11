33353e1983a9f79484c944828107beadaaed351e
Christopher Plumberg <plumberg.1@osu.edu>
2021-05-10 21:33:59 -0500

    Update toy_vn_generator.cpp

--- toy_vn_generator/generator/src/toy_vn_generator.cpp
+++ toy_vn_generator/generator/src/toy_vn_generator.cpp
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 	//flow_generator::eta_high               = 10.0;
 	// --------------------------
 	const int nLoops                       = 10;
-	const int n_events_per_loop            = 10;
+	const int n_events_per_loop            = 10000;
 	flow_generator::N_particles_per_event  = 100;
 	flow_generator::N_events_to_generate   = n_events_per_loop;
 	flow_generator::N_total_events         = nLoops*n_events_per_loop;
